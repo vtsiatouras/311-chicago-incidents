781eea69769525ddcece63a31fceb410051a2e2b
Vangelis Tsiatouras <vangelis.tsiatouras@gmail.com>
2020-11-10 02:00:32 +0200

    [fix]: manipulate dataframe properly & use `batch_size` param at `bulk_create`

--- backend/chicago_incidents/management/commands/import_incidents_from_csvs.py
+++ backend/chicago_incidents/management/commands/import_incidents_from_csvs.py
@@ -5,7 +5,6 @@ import hashlib
 
 from django.core.management.base import BaseCommand, CommandParser
 from django.db import transaction
-from datetime import timezone
 
 from chicago_incidents import models
 
@@ -31,8 +30,10 @@ class Command(BaseCommand):
             self.stdout.write(f"Processing file {input_file}")
             if input_file.endswith('abandoned-vehicles.csv'):
                 self.import_abandoned_vehicles(input_file)
-            elif input_file.endswith('alley-lights-out.csv') or input_file.endswith('street-lights-all-out.csv'):
-                self.import_alley_lights_out_or_street_lights_all_out(input_file)
+            elif input_file.endswith('alley-lights-out.csv'):
+                self.import_alley_lights_out_or_street_lights_all_out(input_file, street_lights=False)
+            elif input_file.endswith('street-lights-all-out.csv'):
+                self.import_alley_lights_out_or_street_lights_all_out(input_file, street_lights=True)
             elif input_file.endswith('street-lights-one-out.csv'):
                 self.import_street_lights_one_out(input_file)
             else:
@@ -56,7 +57,7 @@ class Command(BaseCommand):
                             'ssa', 'latitude', 'longitude', 'location', 'historical_wards_03_15', 'zip_codes',
                             'community_areas', 'census_tracts', 'wards']
 
-        input_df = self.dataframe_normalization(input_df)
+        input_df = self.dataframe_normalization(input_df, models.Incident.ABANDONED_VEHICLE)
 
         incidents = list()
         abandoned_vehicles = list()
@@ -82,7 +83,7 @@ class Command(BaseCommand):
             incident = models.Incident(creation_date=row.creation_date, status=self.get_status_type(row.status),
                                        completion_date=row.completion_date,
                                        service_request_number=row.service_request_number,
-                                       type_of_service_request=self.get_request_type(row.type_of_service_request),
+                                       type_of_service_request=row.type_of_service_request,
                                        current_activity=row.current_activity,
                                        most_recent_action=row.most_recent_action,
                                        street_address=row.street_address, zip_code=row.zip_code,
@@ -95,8 +96,8 @@ class Command(BaseCommand):
                                        longitude=row.longitude, location=row.location)
             incidents.append(incident)
 
-        models.AbandonedVehicle.objects.bulk_create(abandoned_vehicles)
-        models.Incident.objects.bulk_create(incidents)
+        models.AbandonedVehicle.objects.bulk_create(abandoned_vehicles, batch_size=250000)
+        models.Incident.objects.bulk_create(incidents, batch_size=250000)
 
         abandoned_vehicles_incidents = list()
         with transaction.atomic():
@@ -113,8 +114,7 @@ class Command(BaseCommand):
                                                        status=self.get_status_type(row.status),
                                                        completion_date=row.completion_date,
                                                        service_request_number=row.service_request_number,
-                                                       type_of_service_request=self.get_request_type(
-                                                           row.type_of_service_request),
+                                                       type_of_service_request=row.type_of_service_request,
                                                        street_address=row.street_address)
 
                 abandoned_vehicle = models. \
@@ -122,22 +122,30 @@ class Command(BaseCommand):
                                              days_of_report_as_parked=row.days_of_report_as_parked)
                 abandoned_vehicles_incidents.append(abandoned_vehicle)
 
-        models.AbandonedVehicleIncident.objects.bulk_create(abandoned_vehicles_incidents)
+        models.AbandonedVehicleIncident.objects.bulk_create(abandoned_vehicles_incidents, batch_size=250000)
 
-    def import_alley_lights_out_or_street_lights_all_out(self, input_file: str):
+    def import_alley_lights_out_or_street_lights_all_out(self, input_file: str, street_lights: bool):
         """Import the requests for alley lights out or street lights all out (works the same for both of them) to the
         database.
 
         :param input_file: The file from which to load the requests for lights incidents.
+        :param street_lights: Indicator if the method is called for street lights or not
         """
-        self.stdout.write("Getting requests for alley/street lights all out")
+        if street_lights:
+            self.stdout.write("Getting requests for street lights all out")
+        else:
+            self.stdout.write("Getting requests for alley lights out")
+
         input_df = pd.read_csv(input_file, sep=',').replace({np.nan: None})
         input_df.columns = ['creation_date', 'status', 'completion_date', 'service_request_number',
                             'type_of_service_request', 'street_address', 'zip_code', 'x_coordinate', 'y_coordinate',
                             'ward', 'police_district', 'community_area', 'latitude', 'longitude', 'location',
                             'historical_wards_03_15', 'zip_codes', 'community_areas', 'census_tracts', 'wards']
 
-        input_df = self.dataframe_normalization(input_df)
+        if street_lights:
+            input_df = self.dataframe_normalization(input_df, models.Incident.STREET_LIGHTS_ALL_OUT)
+        else:
+            input_df = self.dataframe_normalization(input_df, models.Incident.ALLEY_LIGHTS_OUT)
 
         incidents = list()
 
@@ -145,7 +153,7 @@ class Command(BaseCommand):
             incident = models.Incident(creation_date=row.creation_date, status=self.get_status_type(row.status),
                                        completion_date=row.completion_date,
                                        service_request_number=row.service_request_number,
-                                       type_of_service_request=self.get_request_type(row.type_of_service_request),
+                                       type_of_service_request=row.type_of_service_request,
                                        street_address=row.street_address, zip_code=row.zip_code,
                                        zip_codes=row.zip_codes, x_coordinate=row.x_coordinate,
                                        y_coordinate=row.y_coordinate, ward=row.ward, wards=row.wards,
@@ -155,7 +163,7 @@ class Command(BaseCommand):
                                        latitude=row.latitude, longitude=row.longitude, location=row.location)
             incidents.append(incident)
 
-        models.Incident.objects.bulk_create(incidents)
+        models.Incident.objects.bulk_create(incidents, batch_size=250000)
 
     def import_street_lights_one_out(self, input_file: str):
         """Import the requests for street lights one out to the database.
@@ -168,7 +176,7 @@ class Command(BaseCommand):
                             'type_of_service_request', 'street_address', 'zip_code', 'x_coordinate', 'y_coordinate',
                             'ward', 'police_district', 'community_area', 'latitude', 'longitude', 'location']
 
-        input_df = self.dataframe_normalization(input_df)
+        input_df = self.dataframe_normalization(input_df, models.Incident.STREET_LIGHT_ONE_OUT)
 
         incidents = list()
 
@@ -176,32 +184,38 @@ class Command(BaseCommand):
             incident = models.Incident(creation_date=row.creation_date, status=self.get_status_type(row.status),
                                        completion_date=row.completion_date,
                                        service_request_number=row.service_request_number,
-                                       type_of_service_request=self.get_request_type(row.type_of_service_request),
+                                       type_of_service_request=row.type_of_service_request,
                                        street_address=row.street_address, zip_code=row.zip_code,
                                        x_coordinate=row.x_coordinate, y_coordinate=row.y_coordinate, ward=row.ward,
                                        police_district=row.police_district, community_area=row.community_area,
                                        latitude=row.latitude, longitude=row.longitude, location=row.location)
             incidents.append(incident)
 
-        models.Incident.objects.bulk_create(incidents)
+        models.Incident.objects.bulk_create(incidents, batch_size=250000)
 
     @staticmethod
-    def dataframe_normalization(df: pd.DataFrame) -> pd.DataFrame:
+    def dataframe_normalization(df: pd.DataFrame, request_type: str) -> pd.DataFrame:
         """ Normalizes a given dataframe to a desired condition (removes duplicate rows, convert times to timezone
         aware etc.)
 
         :param df: A pandas dataframe
+        :param request_type: The type of the incident
         :return:  The normalized dataframe
         """
-        df = df.drop_duplicates()
-        df = df[df['type_of_service_request'].notna()]
+        df = df.copy(deep=True)
 
+        df = df.drop_duplicates()
+
+        # Normalize type_of_service_request with the given
+        df['type_of_service_request'] = df['type_of_service_request'].str.replace(r'.+', request_type)
+
+        # Add UTC timezone to datetime fields
         df['creation_date'] = pd.to_datetime(df['creation_date'], errors='ignore')
         df['creation_date'] = df['creation_date'].dt.tz_localize("UTC")
-        df['creation_date'] = df['creation_date'] .replace({np.nan: None})
+        df['creation_date'] = df['creation_date'].astype(object).where(df['creation_date'].notnull(), None)
         df['completion_date'] = pd.to_datetime(df['completion_date'], errors='ignore')
         df['completion_date'] = df['completion_date'].dt.tz_localize("UTC")
-        df['completion_date'] = df['completion_date'] .replace({np.nan: None})
+        df['completion_date'] = df['completion_date'].astype(object).where(df['completion_date'].notnull(), None)
 
         return df
 
@@ -213,16 +227,5 @@ class Command(BaseCommand):
         :return:
         """
         for x in models.Incident.STATUS_TYPE_CHOICES:
-            if x[1] == value:
-                return x[0]
-
-    @staticmethod
-    def get_request_type(value: str) -> str:
-        """
-
-        :param value:
-        :return:
-        """
-        for x in models.Incident.SERVICE_TYPE_CHOICES:
             if x[1] == value:
                 return x[0]
