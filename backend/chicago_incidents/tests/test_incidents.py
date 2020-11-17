d85fe8b1e98aec3a9455c2566bed8f5a760f7060
Vangelis Tsiatouras <vangelis.tsiatouras@gmail.com>
2020-11-17 19:05:30 +0200

    [test]: added tests for carts and potholes create view

--- backend/chicago_incidents/tests/test_incidents.py
+++ backend/chicago_incidents/tests/test_incidents.py
@@ -22,6 +22,56 @@ class IncidentTests(BaseAPITestCase):
         response = self.client.get(reverse('incident-detail', kwargs={'pk': 1}))
         self.assertEqual(response.status_code, status.HTTP_200_OK)
 
+    def test_incident_create(self):
+        """Test that user can create simple incidents
+        """
+        self.authenticate('user')
+
+        data = {'creation_date': '2020-11-15T23:11:07.285Z', 'completion_date': '2020-11-15T23:11:07.285Z',
+                'status': 'OPEN', 'service_request_number': '987654-qwe',
+                'type_of_service_request': 'ABANDONED_VEHICLE', 'street_address': 'Test Address 123', 'zip_code': 0,
+                'zip_codes': 0, 'x_coordinate': 12.134, 'y_coordinate': 12.134, 'latitude': 12.134, 'longitude': 12.134,
+                'ward': 0, 'wards': 0, 'historical_wards_03_15': 0, 'police_district': 0, 'community_area': 0,
+                'community_areas': 0, 'ssa': 0, 'census_tracts': 0}
+
+        response = self.client.post(reverse('incident-list'), data=data, format='json')
+        self.assertEqual(response.status_code, status.HTTP_201_CREATED)
+
+        # Check that we can't create the same incident twice
+        response = self.client.post(reverse('incident-list'), data=data, format='json')
+        self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
+
+    def test_incident_create_malformed_values(self):
+        """Test that user can't input malformed values
+        """
+        self.authenticate('user')
+
+        data = {'creation_date': '2020-11-15T23:11:07.285Z', 'completion_date': '2020-11-15T23:11:07.285Z',
+                'status': 'asdf', 'service_request_number': '987654-qwe',
+                'type_of_service_request': 'ABANDONED_VEHICLE', 'street_address': 'Test Address 123', 'zip_code': 0,
+                'zip_codes': 0, 'x_coordinate': 12.134, 'y_coordinate': 12.134, 'latitude': 12.134, 'longitude': 12.134,
+                'ward': 0, 'wards': 0, 'historical_wards_03_15': 0, 'police_district': 0, 'community_area': 0,
+                'community_areas': 0, 'ssa': 0, 'census_tracts': 0}
+
+        response = self.client.post(reverse('incident-abandoned-vehicle-incident'), data=data, format='json')
+        self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
+        data['status'] = 'OPEN'
+
+        data['type_of_service_request'] = 'unknown type'
+        response = self.client.post(reverse('incident-abandoned-vehicle-incident'), data=data, format='json')
+        self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
+        data['type_of_service_request'] = 'ABANDONED_VEHICLE'
+
+        data['creation_date'] = 'this is not a date'
+        response = self.client.post(reverse('incident-abandoned-vehicle-incident'), data=data, format='json')
+        self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
+        data['type_of_service_request'] = '2020-11-15T23:11:07.285Z'
+
+        data['service_request_number'] = None
+        response = self.client.post(reverse('incident-abandoned-vehicle-incident'), data=data, format='json')
+        self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
+        data['service_request_number'] = '987654-qwe'
+
     def test_abandoned_vehicle_incident_create(self):
         """Test that user can create abandoned vehicle incidents
         """
@@ -63,30 +113,30 @@ class IncidentTests(BaseAPITestCase):
         response = self.client.post(reverse('incident-abandoned-vehicle-incident'), data=data, format='json')
         self.assertEqual(response.status_code, status.HTTP_201_CREATED)
 
+        # Check that we can't create the same incident twice
+        response = self.client.post(reverse('incident-abandoned-vehicle-incident'), data=data, format='json')
+        self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
+
         # Check that we can create incident without activity
         data.pop('activity')
+        data['incident']['service_request_number'] = 'srn1'
         response = self.client.post(reverse('incident-abandoned-vehicle-incident'), data=data, format='json')
         self.assertEqual(response.status_code, status.HTTP_201_CREATED)
-        # Make sure that we didn't create duplicate incident and abandoned vehicle
-        incident = Incident.objects.filter(service_request_number='987654-qwe')
-        self.assertEqual(len(incident), 1)
+        # Make sure that we didn't create duplicate abandoned vehicle
         abandoned_vehicle = AbandonedVehicle.objects.filter(license_plate='123RT45')
         self.assertEqual(len(abandoned_vehicle), 1)
 
         # Check that we can create incident without abandoned vehicle & activity
         data.pop('abandoned_vehicle')
+        data['incident']['service_request_number'] = 'srn2'
         response = self.client.post(reverse('incident-abandoned-vehicle-incident'), data=data, format='json')
         self.assertEqual(response.status_code, status.HTTP_201_CREATED)
-        # Make sure that we didn't create duplicate incident
-        incident = Incident.objects.filter(service_request_number='987654-qwe')
-        self.assertEqual(len(incident), 1)
 
-        data.update({'activity': {'current_activity': 'Processing request','most_recent_action': 'Get info'}})
+        data.update({'activity': {'current_activity': 'Processing request', 'most_recent_action': 'Get info'}})
+        data['incident']['service_request_number'] = 'srn3'
         response = self.client.post(reverse('incident-abandoned-vehicle-incident'), data=data, format='json')
         self.assertEqual(response.status_code, status.HTTP_201_CREATED)
-        # Make sure that we didn't create duplicate incident and activity
-        incident = Incident.objects.filter(service_request_number='987654-qwe')
-        self.assertEqual(len(incident), 1)
+        # Make sure that we didn't create duplicate activity
         activity = Activity.objects.filter(current_activity='Processing request')
         self.assertEqual(len(activity), 1)
 
@@ -129,9 +179,6 @@ class IncidentTests(BaseAPITestCase):
             }
         }
 
-        response = self.client.post(reverse('incident-abandoned-vehicle-incident'), data=data, format='json')
-        self.assertEqual(response.status_code, status.HTTP_201_CREATED)
-
         data['incident']['status'] = 'asdf'
         response = self.client.post(reverse('incident-abandoned-vehicle-incident'), data=data, format='json')
         self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
@@ -166,4 +213,143 @@ class IncidentTests(BaseAPITestCase):
         data['abandoned_vehicle']['vehicle_make_model'] = None
         data['abandoned_vehicle']['vehicle_color'] = None
         response = self.client.post(reverse('incident-abandoned-vehicle-incident'), data=data, format='json')
+        self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
+
+    def test_potholes_and_carts_incident_create(self):
+        """Test that user can create garbage carts and potholes incidents
+        """
+        self.authenticate('user')
+
+        data = {
+            'incident': {
+                'creation_date': '2020-11-15T23:11:07.285Z',
+                'completion_date': '2020-11-15T23:11:07.285Z',
+                'status': 'OPEN',
+                'service_request_number': '987654-qwe',
+                'type_of_service_request': 'POT_HOLE',
+                'street_address': 'Test Address 123',
+                'zip_code': 0,
+                'zip_codes': 0,
+                'x_coordinate': 12.134,
+                'y_coordinate': 12.134,
+                'latitude': 12.134,
+                'longitude': 12.134,
+                'ward': 0,
+                'wards': 0,
+                'historical_wards_03_15': 0,
+                'police_district': 0,
+                'community_area': 0,
+                'community_areas': 0,
+                'ssa': 0,
+                'census_tracts': 0
+            },
+            'activity': {
+                'current_activity': 'Processing request',
+                'most_recent_action': 'Get info'
+            },
+            'carts_and_potholes': {
+                'number_of_elements': '100',
+            }
+        }
+        response = self.client.post(reverse('incident-garbage-carts-and-potholes-incident'), data=data, format='json')
+        self.assertEqual(response.status_code, status.HTTP_201_CREATED)
+
+        # Check for 'GARBAGE_CART' as type of request
+        data['incident']['type_of_service_request'] = 'GARBAGE_CART'
+        response = self.client.post(reverse('incident-garbage-carts-and-potholes-incident'), data=data, format='json')
+        self.assertEqual(response.status_code, status.HTTP_201_CREATED)
+
+        # Check that we can't create the same incident twice
+        response = self.client.post(reverse('incident-garbage-carts-and-potholes-incident'), data=data, format='json')
+        self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
+
+        # Check that we can create incident without activity
+        data.pop('activity')
+        data['incident']['service_request_number'] = 'srn1'
+        response = self.client.post(reverse('incident-garbage-carts-and-potholes-incident'), data=data, format='json')
+        self.assertEqual(response.status_code, status.HTTP_201_CREATED)
+
+        # Check that we can create incident without carts-and-potholes & activity
+        data.pop('carts_and_potholes')
+        data['incident']['service_request_number'] = 'srn2'
+        response = self.client.post(reverse('incident-garbage-carts-and-potholes-incident'), data=data, format='json')
+        self.assertEqual(response.status_code, status.HTTP_201_CREATED)
+
+        data.update({'activity': {'current_activity': 'Processing request', 'most_recent_action': 'Get info'}})
+        data['incident']['service_request_number'] = 'srn3'
+        response = self.client.post(reverse('incident-garbage-carts-and-potholes-incident'), data=data, format='json')
+        self.assertEqual(response.status_code, status.HTTP_201_CREATED)
+        # Make sure that we didn't create duplicate activity
+        activity = Activity.objects.filter(current_activity='Processing request')
+        self.assertEqual(len(activity), 1)
+
+    def test_potholes_and_carts_incident_create_malformed_values(self):
+        """Test that user can't input malformed values
+        """
+        self.authenticate('user')
+
+        data = {
+            'incident': {
+                'creation_date': '2020-11-15T23:11:07.285Z',
+                'completion_date': '2020-11-15T23:11:07.285Z',
+                'status': 'OPEN',
+                'service_request_number': '987654-qwe',
+                'type_of_service_request': 'POT_HOLE',
+                'street_address': 'Test Address 123',
+                'zip_code': 0,
+                'zip_codes': 0,
+                'x_coordinate': 12.134,
+                'y_coordinate': 12.134,
+                'latitude': 12.134,
+                'longitude': 12.134,
+                'ward': 0,
+                'wards': 0,
+                'historical_wards_03_15': 0,
+                'police_district': 0,
+                'community_area': 0,
+                'community_areas': 0,
+                'ssa': 0,
+                'census_tracts': 0
+            },
+            'activity': {
+                'current_activity': 'Processing request',
+                'most_recent_action': 'Get info'
+            },
+            'carts_and_potholes': {
+                'number_of_elements': '100',
+            }
+        }
+
+        data['incident']['status'] = 'asdf'
+        response = self.client.post(reverse('incident-garbage-carts-and-potholes-incident'), data=data, format='json')
+        self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
+        data['incident']['status'] = 'OPEN'
+
+        data['incident']['type_of_service_request'] = 'unknown type'
+        response = self.client.post(reverse('incident-garbage-carts-and-potholes-incident'), data=data, format='json')
+        self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
+        data['incident']['type_of_service_request'] = 'POT_HOLE'
+
+        data['incident']['type_of_service_request'] = 'TREE_TRIMS'
+        response = self.client.post(reverse('incident-garbage-carts-and-potholes-incident'), data=data, format='json')
+        self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
+        data['incident']['type_of_service_request'] = 'POT_HOLE'
+
+        data['incident']['creation_date'] = 'this is not a date'
+        response = self.client.post(reverse('incident-garbage-carts-and-potholes-incident'), data=data, format='json')
+        self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
+        data['incident']['type_of_service_request'] = '2020-11-15T23:11:07.285Z'
+
+        data['incident']['service_request_number'] = None
+        response = self.client.post(reverse('incident-garbage-carts-and-potholes-incident'), data=data, format='json')
+        self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
+        data['incident']['service_request_number'] = '987654-qwe'
+
+        data['activity']['current_activity'] = None
+        data['activity']['most_recent_action'] = None
+        response = self.client.post(reverse('incident-garbage-carts-and-potholes-incident'), data=data, format='json')
+        self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
+
+        data['carts_and_potholes']['number_of_elements'] = None
+        response = self.client.post(reverse('incident-garbage-carts-and-potholes-incident'), data=data, format='json')
         self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
