15ed4eec622b03ae944c06a294e9b75eee180e2e
Vangelis Tsiatouras <vangelis.tsiatouras@gmail.com>
2020-11-18 01:14:35 +0200

    [refactor]: removed some redundant code

--- backend/chicago_incidents/serializers/carts_and_potholes.py
+++ backend/chicago_incidents/serializers/carts_and_potholes.py
@@ -5,19 +5,10 @@ from rest_framework.serializers import ModelSerializer
 from ..models import NumberOfCartsAndPotholes
 
 
-class CartsAndPotholesSerializer(ModelSerializer):
+class CartsAndPotholesCreateSerializer(ModelSerializer):
     """Carts and potholes create serializer
     """
 
     class Meta:
         model = NumberOfCartsAndPotholes
         fields = ['number_of_elements']
-
-
-class CartsAndPotholesSerializerForIncident(CartsAndPotholesSerializer):
-    """Carts and potholes create serializer that is used when creating incidents
-    """
-
-    def get_unique_together_validators(self):
-        """Overriding method to disable unique together checks"""
-        return []
