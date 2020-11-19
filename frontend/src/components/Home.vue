abfa9696d6ef641e8101c866f91a37ef507341b6
Vangelis Tsiatouras <vangelis.tsiatouras@gmail.com>
2020-11-19 02:04:37 +0200

    [refactor]: removed some unused code from Home.vue

--- frontend/src/components/Home.vue
+++ frontend/src/components/Home.vue
@@ -1,14 +1,12 @@
 <template>
   <div class="container">
     <header class="jumbotron">
-      <h3>{{content}}</h3>
+      <h3>Chicago Incidents Actions</h3>
     </header>
   </div>
 </template>
 
 <script>
-import UserService from '../services/user.service';
-
 export default {
   name: 'Home',
   data() {
@@ -16,18 +14,6 @@ export default {
       content: ''
     };
   },
-  mounted() {
-    UserService.getPublicContent().then(
-        response => {
-          this.content = response.data;
-        },
-        error => {
-          this.content =
-              (error.response && error.response.data) ||
-              error.message ||
-              error.toString();
-        }
-    );
-  }
+  mounted() {}
 };
 </script>
