256c562bc0e0c463c227f9394283081f8b295eef
Vangelis Tsiatouras <vangelis.tsiatouras@gmail.com>
2020-11-19 02:04:56 +0200

    [feat]: load username at the navbar

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -2,10 +2,11 @@
   <div id="app">
     <nav class="navbar navbar-expand navbar-dark bg-dark">
       <a href class="navbar-brand" @click.prevent>311 Chicago Incidents</a>
-      <div class="navbar-nav mr-auto">
+      <div v-if="currentUser" class="navbar-nav mr-auto">
         <li class="nav-item">
           <router-link to="/home" class="nav-link">
-            <font-awesome-icon icon="home" />Home
+            <font-awesome-icon icon="home"/>
+            Home
           </router-link>
         </li>
       </div>
@@ -13,12 +14,14 @@
       <div v-if="!currentUser" class="navbar-nav ml-auto">
         <li class="nav-item">
           <router-link to="/register" class="nav-link">
-            <font-awesome-icon icon="user-plus" />Sign Up
+            <font-awesome-icon icon="user-plus"/>
+            Sign Up
           </router-link>
         </li>
         <li class="nav-item">
           <router-link to="/login" class="nav-link">
-            <font-awesome-icon icon="sign-in-alt" />Login
+            <font-awesome-icon icon="sign-in-alt"/>
+            Login
           </router-link>
         </li>
       </div>
@@ -26,28 +29,39 @@
       <div v-if="currentUser" class="navbar-nav ml-auto">
         <li class="nav-item">
           <router-link to="/profile" class="nav-link">
-            <font-awesome-icon icon="user" />
-            {{ currentUser.username }}
+            <font-awesome-icon icon="user"/>
+            {{ userName }}
           </router-link>
         </li>
         <li class="nav-item">
           <a class="nav-link" href @click.prevent="logOut">
-            <font-awesome-icon icon="sign-out-alt" />LogOut
+            <font-awesome-icon icon="sign-out-alt"/>
+            LogOut
           </a>
         </li>
       </div>
     </nav>
 
     <div class="container">
-      <router-view />
+      <router-view/>
     </div>
   </div>
 </template>
 
 <script>
+import UserService from "@/services/user.service";
+
 export default {
+  data() {
+    return {
+      userId: null,
+      userName: null,
+      userEmail: null
+    }
+  },
   computed: {
     currentUser() {
+      [this.userId, this.userName, this.userEmail] = UserService.getUserInfoFromToken()
       return this.$store.state.auth.user;
     }
   },
