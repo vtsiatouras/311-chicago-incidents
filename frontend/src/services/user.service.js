0da0bc312c1ed98026642a78a9f2a99c0dbfeaec
Vangelis Tsiatouras <vangelis.tsiatouras@gmail.com>
2020-11-19 02:06:07 +0200

    [feat]: added service method that returns user info from jwt

--- frontend/src/services/user.service.js
+++ frontend/src/services/user.service.js
@@ -1,11 +1,23 @@
 import axios from 'axios';
 import authHeader from './auth-header';
+import jwt_decode from "jwt-decode";
 
 const API_URL = 'http://localhost:8000/api/v1/';
 
 class UserService {
-    getUserInfo() {
+    getUserInfoFromAPI() {
         return axios.get(API_URL + 'users/', { headers: authHeader() });
+    }
+
+    getUserInfoFromToken(){
+        try {
+            let user = JSON.parse(localStorage.getItem('user'));
+            const decoded = jwt_decode(user.access)
+            return [decoded.user_id, decoded.user_name, decoded.user_email]
+        }
+        catch(error){
+            return [ null, null, null ]
+        }
     }
 }
 
