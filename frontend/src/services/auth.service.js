c174b70adf1889a6ec1a86fdc897d3da78d44a96
Vangelis Tsiatouras <vangelis.tsiatouras@gmail.com>
2020-11-20 18:34:53 +0200

    [feat]: front end now refreshes JWT when the access token is expired

--- frontend/src/services/auth.service.js
+++ frontend/src/services/auth.service.js
@@ -22,6 +22,22 @@ class AuthService {
         localStorage.removeItem('user');
     }
 
+    getNewToken() {
+        let user = JSON.parse(localStorage.getItem('user'));
+        return new Promise((resolve, reject) => {
+            axios
+                .post(API_URL + 'auth/refresh/', { refresh: user.refresh })
+                .then(response => {
+                    user['access'] = response.data['access']
+                    localStorage.setItem("user", JSON.stringify(user));
+                    resolve(response.data.access);
+                })
+                .catch((error) => {
+                    reject(error);
+                });
+        });
+    }
+
     register(user) {
         return axios.post(API_URL + 'users', {
             username: user.username,
